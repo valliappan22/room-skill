823557300d23f15fcb88b095ad9378a9c63640bf
valliappan22 <valliappan445@gmail.com>
2020-03-17 02:21:25 +0530

    check

--- __init__.py
+++ __init__.py
@@ -8,11 +8,13 @@ class Room(MycroftSkill):
     @intent_file_handler('room.intent')
     def handle_room(self, message):
         roomname = message.data.get('roomname')
+        if roomname != None:
+            self.speak_dialog('room', data={
+                'roomname': roomname
+            })
+        else:
 
-        self.speak_dialog('room', data={
-            'roomname': roomname
-        })
-
+            self.speak_dialog('test')
 
 def create_skill():
     return Room()
