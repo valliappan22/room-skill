51327864c27065e80b557b10a4e47ff59250b19a
valliappan22 <valliappan445@gmail.com>
2020-03-17 09:51:16 +0530

    checking

--- __init__.py
+++ __init__.py
@@ -9,13 +9,20 @@ class Room(MycroftSkill):
     def handle_room(self, message):
         roomname = message.data.get('roomname')
         if roomname != None:
-            self.speak_dialog('room', data={
-                'roomname': roomname
-            })
+            self.speak_dialog('room', data={'roomname': roomname})
             self.speak_dialog('repeat')
             response = self.ask_yesno('')
             if response == 'yes':
-                self.speak_dialog('day')
+                self.speak_dialog('room', data={'roomname': roomname})
+                self.speak_dialog('anything')
+                 resp = self.ask_yesno('')
+                 if resp == 'no':
+                     self.speak_dialog('day')
+            else:
+                 self.speak_dialog('anything')
+                 resp = self.ask_yesno('')
+                 if resp == 'no':
+                     self.speak_dialog('day')
 
         else:
             self.speak_dialog('test')
